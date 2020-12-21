923d4e9aac6d84b13ef56ebd535cce5736aa76fa
alminana <alminanaeilerbutad@yahoo.com>
2020-12-22 02:39:56 +0800

    Updates

---
+++ precache-manifest.48e8eb68e960afe7c7f3df7b50f27157.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8d022838c94cf0cc133de23c1b1068",
+    "url": "/Samsung/index.html"
+  },
+  {
+    "revision": "94fffb56eae8c330c8d2",
+    "url": "/Samsung/static/css/main.c1476892.chunk.css"
+  },
+  {
+    "revision": "8f0aac18e45bf5525a34",
+    "url": "/Samsung/static/js/2.6ed1760b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Samsung/static/js/2.6ed1760b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94fffb56eae8c330c8d2",
+    "url": "/Samsung/static/js/main.2d4494b2.chunk.js"
+  },
+  {
+    "revision": "d9bc25e6a6bafdd62078",
+    "url": "/Samsung/static/js/runtime-main.e3fc0c9d.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/Samsung/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/Samsung/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/Samsung/static/media/times-solid.2c5552a1.svg"
+  }
+]);
